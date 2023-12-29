1a19b3f08024b33b8184212a13854251b91a4a6c
zain4471 <zainy9505@gmail.com>
2023-12-29 16:44:55 +0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,17 @@
 module.exports = {
   siteMetadata: {
     title: `GATSBY_CMS`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `pkc8od7g9jjq`,
+        accessToken: `_U8HynuS6od0Xm9F-m00aL4TwbxLff4E9ACU8dzL7G0`,
+      },
+    },
+    `gatsby-plugin-image`,
+  ],
 };
